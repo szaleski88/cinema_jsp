4ed23155996d45ea3908d0084d5d53bdd8aa8785
zaleski <s.zaleski88@gmail.com>
2018-05-09 19:08:59 +0200

    Uzytkownik utworzony

--- src/main/java/com/sda/model/Uzytkownik.java
+++ src/main/java/com/sda/model/Uzytkownik.java
@@ -1,4 +1,17 @@
 package com.sda.model;
 
-public class Uzytkownik {
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public final class Uzytkownik {
+
+    private String login;
+    private String haslo;
+    private String imie;
+    private String nazwisko;
+
 }
