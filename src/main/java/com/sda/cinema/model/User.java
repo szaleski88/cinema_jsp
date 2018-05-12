f03c748c92e9bce95266935e2218094258ec685b
Tomasz Krakowiak <tom_krakowiak@wp.pl>
2018-05-12 12:15:49 +0200

    Walidaacja formularza rejestracji

--- src/main/java/com/sda/cinema/model/User.java
+++ src/main/java/com/sda/cinema/model/User.java
@@ -14,8 +14,17 @@ public final class User {
     private String repeatedPassword;
     private String firstName;
     private String lastName;
-
     private String email;
     private String registerDate;
     private String amountOfPoints;
+
+    public boolean validateForm() {
+
+        return (firstName.matches("[A-Za-złśżźćęąŁŚĄŻŹĆĘ]+"))
+                && lastName.matches("[A-Za-złśżźćęąŁŚĄŻŹĆĘ]+")
+                && email.matches("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}")
+                && login.matches("[A-Za-z_0-9]{6,}")
+                && password.matches("[A-Za-z_0-9.!]{6,}")
+                && !password.equals(repeatedPassword);
+    }
 }
